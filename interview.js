61d6fd1f926adab9362de3dea43e56e9798c1c06
Hezron-tech <kiprophez@gmail.com>
2022-10-21 14:10:38 +0300

    creating js object

---
+++ interview.js
@@ -0,0 +1,11 @@
+//creating object 
+
+var person={
+    name:'hezron',
+    age:23,
+    career:'Software dev'
+    
+}
+console.log(person.name);
+console.log(person.age);
+console.log(person.career);
